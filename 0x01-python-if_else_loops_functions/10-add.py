9f406db999f807a3f0c1702187179ce6e2f7ca10
Lukman Asinmi <asinmilukman@gmail.com>
2022-11-29 14:54:20 -0800

    All tasks completed

---
+++ 0x01-python-if_else_loops_functions/10-add.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python3
+# Author - Asinmi Lukman
+def add(a, b):
+    """Return addition of a and b."""
+    return (a + b)
---
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Author - Asinmi Lukman
+def pow(a, b):
+    return (a ** b)
